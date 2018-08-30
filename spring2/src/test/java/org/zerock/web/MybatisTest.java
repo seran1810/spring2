63f0846c7befc575a2a4be31c9bc177ce2f3b591
kosta <kosta@kosta-PC>
2018-08-30 18:42:38 +0900

    08.30

---
+++ spring2/src/test/java/org/zerock/web/MybatisTest.java
@@ -0,0 +1,26 @@
+package org.zerock.web;
+
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class MybatisTest {
+	@Inject
+	private SqlSessionFactory sqlFactory;
+
+	@Test
+	
+	public void test() {
+		assertNotNull(sqlFactory);
+		
+	}
+
+}
